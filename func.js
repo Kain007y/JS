42460dfe7d8ecef158cb4bcecc9348baedb23372
Ilya Pershikov <ilya.pershikov.02@gmail.com>
2025-02-26 10:35:57 +0700

    Message

--- func.js
+++ func.js
@@ -1,2 +1,8 @@
-const a = 5;
-console.log(a+5);
+const http = require("http");
+http.createServer(function(request,response){
+     
+    response.end("Hello NodeJS!");
+     
+}).listen(3000, "127.0.0.1",function(){
+    console.log("Сервер начал прослушивание запросов на порту 3000");
+});
